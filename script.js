1d2969492eb1444a6aa957977c2c8b3342a4982f
Shriram kushwaha <117708615+shri214@users.noreply.github.com>
2023-04-14 11:29:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let students={
+	name="Jhon Deo"
+}
+	Object.prototype.getKeys=function(){
+	 return Object.keys(this);
+	}
+let key=students.getKeys();
+console.log(key);
+	
